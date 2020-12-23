9d148625df09fc4131e4117f7030a2a982dd6f03
Rhysn <cedaren@gmail.com>
2020-12-23 21:33:11 +0800

    优化

--- jd_crazy_joy.js
+++ jd_crazy_joy.js
@@ -282,7 +282,7 @@ function doApplyJdBean(bean = 1000) {
     })
   })
 }
-function getUserInfo(code = "EdLPh8A6X5G1iWXu-uPYfA==") {
+function getUserInfo(code = "L1gAlidA1wJx_uNrwl2R0Kt9zd5YaBeE") {
   let body = {"paramData": {"inviter": code}}
   return new Promise(async resolve => {
     $.get(taskUrl('crazyJoy_user_gameState', JSON.stringify(body)), async (err, resp, data) => {
