45a1f4f4b0980dac5e114a34a035253893f5399a
Rhysn <cedaren@gmail.com>
2020-12-30 02:55:01 +0800

    .

--- jd_task_sharecode.js
+++ jd_task_sharecode.js
@@ -224,7 +224,7 @@ function getTaskSharecode(homedata, appId, taskType) {
         } else {
           if (safeGet(data)) {
             data = JSON.parse(data);
-            console.log(data.data);
+            console.log(data);
             if (data.data.bizCode === 0) {
               $.taskVos.map(item => {
                 if (item.taskType === taskType) {
