dfbf7135b326ebf78fe03f025021055dd85eff2d
Rhysn <cedaren@gmail.com>
2021-01-21 07:25:12 +0800

    .

--- jd_fruit.js
+++ jd_fruit.js
@@ -1458,7 +1458,7 @@ function jsonParse(str) {
 }
 async function cleanFriendList(){
     await friendListInitForFarm();//查询好友列表
-    while ($.friendList.friends && $.friendList.friends.length > 30) {
+    while ($.friendList.friends && $.friendList.friends.length > 10) {
       for (let friend of $.friendList.friends) {
         if(theShareCodes.includes(friend.shareCode)) continue;
         console.log(`\n开始删除好友 [${friend.shareCode}]`);
