2a7f0a8a0aaa7dff2ac05cc1ce9eec02af919841
Rhysn <cedaren@gmail.com>
2021-05-01 10:47:45 +0800

    .

--- jd_task_sharecode.js
+++ jd_task_sharecode.js
@@ -1,6 +1,6 @@
 /*
 抽奖机活动分享码获取
-bash -c "exec 1<>/proc/1/fd/1; exec 2<>/proc/1/fd/2; set -o allexport; source /all; source /env; source /cookies; node /scripts/jd_task_sharecode.js > /logs/jd_task_sharecode.log 2>&1 | sed 's/^/ 【获取活动分享码】/'; "
+bash -c "exec 1<>/proc/1/fd/1; exec 2<>/proc/1/fd/2; set -o allexport; source /overall; source /env; source /cookies; node /scripts/jd_task_sharecode.js > /logs/jd_task_sharecode.log 2>&1 | sed 's/^/ 【获取活动分享码】/'; "
  */
 const $ = new Env('抽奖机活动分享码');
 
